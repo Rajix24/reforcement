4451562e16191d8b83080d546941b97a506a4158
Younes Raji <rajix2100@gmail.com>
2026-04-05 18:30:59 -0400

    first week

--- jeudi/challange_1/main.js
+++ jeudi/challange_1/main.js
@@ -1,2 +1,32 @@
-import {  } from "../../Globle-functions/functions.js";
-import {  } from "./helper.js"
+import {} from "../../Globle-functions/functions.js";
+import {} from "./helper.js";
+
+const reservations = [
+  {
+    id: 1,
+    nom_client: "Ahmed",
+    nombre_personnes: 4,
+    date: "2026-04-05",
+    heure: "20:00",
+    statut: "confirmée",
+    telephone: "0600000000",
+  },
+  {
+    id: 2,
+    nom_client: "Sara",
+    nombre_personnes: 2,
+    date: "2026-04-05",
+    heure: "20:00",
+    statut: "en attente",
+    telephone: "0611111111",
+  },
+  {
+    id: 3,
+    nom_client: "Youssef",
+    nombre_personnes: 6,
+    date: "2026-04-06",
+    heure: "21:00",
+    statut: "annulée",
+    telephone: "0622222222",
+  },
+];
